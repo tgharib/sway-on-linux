1cef8ca63ec133e261ec39a3d2d65c00b02610e1
Taha Gharib <tgharib@users.noreply.github.com>
2023-06-05 20:35:33 -0700

    Create sway-desktop-launcher.sh

---
+++ .scripts/.scripts/sway-desktop-launcher.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+setterm -linewrap off
+
+paths_spaced="/home/owner/.local/share/applications/ /nix/var/nix/profiles/per-user/owner/profile/share/applications/ /run/current-system/sw/share/applications/ /var/lib/flatpak/exports/share/applications/ /usr/share/applications/ /var/lib/snapd/desktop/applications/"
+name=$(fd -e desktop . $paths_spaced | sd ".+/(.+)\.desktop" "\$1" | sort -u | fzf --prompt='Launch app: ')
+
+IFS=" "
+for path in $paths_spaced; do
+  swaymsg exec dex "$path$name.desktop"
+  # to avoid executing in all 3 paths, we can stop at first success or keep the index number from fzf return or for each fzf entry, we can have a prepended id
+done
+
+sleep 0.1
